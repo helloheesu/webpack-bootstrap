d53c62734ebf93538fad33a9ab10a4929c3f9f83
helloheesu <heesu.jung.developer@gmail.com>
2019-11-02 00:08:37 +0900

    :heavy_plus_sign: clean-webpack-plugin, html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-  mode: "development"
+  mode: "development",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      meta: {
+        viewport:
+          "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+      }
+    })
+  ]
 };
