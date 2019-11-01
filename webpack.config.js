451079317b9369f6cb862833cafb357fe955a0ad
helloheesu <heesu.jung.developer@gmail.com>
2019-11-02 00:08:49 +0900

    :heavy_plus_sign: babel, babel-loader, @babel/preset-react
    babel-loader : babel + webpack
    @babel/preset-react : babel + react

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  entry: "./src/index.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
