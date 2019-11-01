22dd3af475305c4d47bc888c50acd7a303f7dbdf
helloheesu <heesu.jung.developer@gmail.com>
2019-11-02 00:08:47 +0900

    :heavy_plus_sign: webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -12,5 +12,8 @@ module.exports = {
           "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
       }
     })
-  ]
+  ],
+  devServer: {
+    open: true
+  }
 };
